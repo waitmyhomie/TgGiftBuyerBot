6cb5af0c21ccdb0497bf27914d6b62aba799cc7d
waitmyhomie <jabs@faber-solartechnik.de>
2025-06-30 15:53:49 +0200

    token

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 def load_config():
     return {
-        "bot_token": 'ВАШ_НОВЫЙ_ТОКЕН',  # Обязательно смените!
+        "bot_token": '7820577888:AAFH_YOr6-8uZDzfKnOEOfkMYaewp6J0QN0',  # Обязательно смените!
         "DATABASE_URL": 'sqlite:///database.db',
         "OWNER_ID": 1487757625,  # Ваш ID
         "PRIVATE_MODE": True     # Приватный режим
